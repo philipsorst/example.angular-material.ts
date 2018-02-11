ef2354af25dc5a4e131e2c154a1f9e50200bedda
Philip Washington Sorst <philip@sorst.net>
2018-02-11 22:13:21 +0100

    Adding and removing list items

--- src/list/list.component.ts
+++ src/list/list.component.ts
@@ -7,17 +7,64 @@ import {Title} from "@angular/platform-browser";
 })
 export class ListComponent implements OnInit
 {
-    public userNames: string[] = [];
+    public users: User[] = [];
 
     constructor(private titleService: Title)
     {
     }
 
+    /**
+     * @override
+     */
     public ngOnInit(): void
     {
         this.titleService.setTitle('List');
+        let users = [];
         for (let i = 0; i < 50; i++) {
-            this.userNames.push(faker.name.findName());
+            users.push(this.createUser());
         }
+
+        users.sort((user1: User, user2: User) => {
+            return user1.username.localeCompare(user2.username);
+        });
+
+        this.users = users;
     }
+
+    public trackByUuid(user: User)
+    {
+        return user.id;
+    }
+
+    public add()
+    {
+        this.users.push(this.createUser());
+        this.users.sort((user1: User, user2: User) => {
+            return user1.username.localeCompare(user2.username);
+        });
+    }
+
+    public remove(user: User)
+    {
+        this.users = this.users.filter((listUser: User) => {
+            return listUser.id !== user.id;
+        });
+    }
+
+    private createUser(): User
+    {
+        let user = new User();
+        user.id = faker.random.uuid();
+        user.username = faker.name.findName();
+        user.avatarUrl = faker.image.avatar();
+
+        return user;
+    }
+}
+
+export class User
+{
+    public id: string;
+    public username: string;
+    public avatarUrl: string;
 }
